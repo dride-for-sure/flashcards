8acd117e1531540d17aa1bbe7f0ba026100c16d7
dennisjauernig <dennis@jauernig.email>
2021-02-19 20:37:58 +0100

    Remove gameMode conditional rendering

--- src/components/Cards/StartGame/StartGame.js
+++ src/components/Cards/StartGame/StartGame.js
@@ -1,27 +1,22 @@
 import PropTypes from 'prop-types';
 import { Container, Difficulty } from './styles';
 
-export default function StartGame({ gameMode, onStartGameClick }) {
-  const disabled = gameMode !== 'lobby';
-
+export default function StartGame({ onStartGameClick }) {
   return (
-    <Container disabled={disabled} gameMode={gameMode}>
+    <Container>
       <h1>To start choose a level</h1>
       <span>
         <Difficulty
-          disabled={disabled}
           onClick={() => { onStartGameClick('easy'); }}
           title="Easy peasy">
           🥱
         </Difficulty>
         <Difficulty
-          disabled={disabled}
           onClick={() => { onStartGameClick('moderat'); }}
           title="For everyday">
           💪
         </Difficulty>
         <Difficulty
-          disabled={disabled}
           onClick={() => { onStartGameClick('hard'); }}
           title="100% pain">
           🤯
@@ -32,6 +27,5 @@ export default function StartGame({ gameMode, onStartGameClick }) {
 }
 
 StartGame.propTypes = {
-  gameMode: PropTypes.string.isRequired,
   onStartGameClick: PropTypes.func.isRequired,
 };
