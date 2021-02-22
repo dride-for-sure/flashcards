7f81eae94b12acbeeb7d1f3b06ac230004b88296
dennisjauernig <dennis@jauernig.email>
2021-02-22 21:07:45 +0100

    :construction: Restructure for Websocks / API
    Implement App.js > Lobby > new Websocks / API Structure

--- src/components/Cards/PlayerList/PlayerInfos/SetPlayerName.js
+++ src/components/Cards/PlayerList/PlayerInfos/SetPlayerName.js
@@ -4,17 +4,17 @@ import uuid from '../../../../common/uuid';
 import Player from '../Player/Player';
 import { Container, Input } from './styles';
 
-export default function SetPlayerName({ thisPlayer, setThisPlayer }) {
+export default function SetPlayerName({ player, setPlayer }) {
   const [playerName, setPlayerName] = useState('');
   const headline = 'Your ninja name?';
   const inputPlaceholder = 'It has one, right?';
 
-  const hasThisPlayer = thisPlayer && Object.keys(thisPlayer).length === 0;
+  const hasThisPlayer = player && Object.keys(player).length === 0;
   const hasPlayerName = playerName.length > 0 && playerName.length < 25;
   const handleSubmit = (event) => {
     event.preventDefault();
     if (hasPlayerName) {
-      setThisPlayer({ id: uuid(), name: playerName });
+      setPlayer({ id: uuid(), name: playerName });
       setPlayerName('');
     }
   };
@@ -30,16 +30,16 @@ export default function SetPlayerName({ thisPlayer, setThisPlayer }) {
       </Container>
       )}
       { !hasThisPlayer && (
-      <Player key={thisPlayer.id} player={thisPlayer} />
+      <Player key={player.id} player={player} />
       )}
     </>
   );
 }
 
 SetPlayerName.propTypes = {
-  thisPlayer: PropTypes.shape({
+  player: PropTypes.shape({
     id: PropTypes.string,
     name: PropTypes.string,
   }).isRequired,
-  setThisPlayer: PropTypes.func.isRequired,
+  setPlayer: PropTypes.func.isRequired,
 };
