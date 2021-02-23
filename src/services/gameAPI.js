b3ec12ce49fffa0ce216be1b54755032b1975f9b
dennisjauernig <dennis@jauernig.email>
2021-02-23 19:35:29 +0100

    :construction: Work on API
    Change props, add .then to .put method

--- src/services/gameAPI.js
+++ src/services/gameAPI.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
-export const joinGame = (player) => axios.post('/api/lobby', player)
+export const joinGame = (player) => axios
+  .post('/api/lobby', player)
   .then((response) => response.data)
-  .catch((error) => console.log('POST:', error));
+  .catch((error) => {
+    console.log('JOIN GAME:', error);
+    return '';
+  });
 
 export const sendAnswer = (player, game, cards) => axios
   .put(`/api/games/${game.id}/${player.id}`, cards)
-  .catch((error) => console.log('PUT:', error));
+  .then((response) => response.data)
+  .catch((error) => {
+    console.log('SEND ANSWER:', error);
+  });
 
 export const startGame = (player, game, level) => axios
-  .post(`/api/games/${game.id}/${player.id}`, { id: game.id, level });
+  .post(`/api/games/${game.id}/${player.id}`, { id: game.id, level })
+  .catch((error) => {
+    console.log('START GAME:', error);
+  });
