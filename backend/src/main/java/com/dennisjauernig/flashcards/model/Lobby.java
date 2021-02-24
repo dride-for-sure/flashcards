9206bb62a0d7dddb9db885d36fce09a94c4f6f83
dennisjauernig <dennis@jauernig.email>
2021-02-24 09:56:08 +0100

    :construction: Websocks
    Implement MessageMapping and simpMessagingTemplate for all game states and requests

--- backend/src/main/java/com/dennisjauernig/flashcards/model/Lobby.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/model/Lobby.java
@@ -1,20 +1,19 @@
 package com.dennisjauernig.flashcards.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class StartGameDto {
+@AllArgsConstructor
+public class Lobby {
 
  private UUID uuid;
- private int level;
- private UUID gameMaster;
+ private GameStatus status;
+ private List<Player> players;
 
 }
---
+++ backend/src/main/java/com/dennisjauernig/flashcards/services/PlayService.java
@@ -0,0 +1,10 @@
+package com.dennisjauernig.flashcards.services;
+
+import com.dennisjauernig.flashcards.model.StartGameDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlayService {
+ public void startGame (StartGameDto dto) {
+ }
+}
