9b8832a0467b6ab42d2ebe669ed89d656286163f
dennisjauernig <dennis@jauernig.email>
2021-02-25 23:46:24 +0100

    :construction: Annotations for upcoming @ConfigurationProperties

--- backend/src/main/java/com/dennisjauernig/flashcards/FlashCardsApplication.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/FlashCardsApplication.java
@@ -2,8 +2,10 @@ package com.dennisjauernig.flashcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class FlashCardsApplication {
 
  public static void main (String[] args) {
