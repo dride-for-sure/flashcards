dccd792de30da5908f4fbc546a9d4e965c8a8eff
dennisjauernig <dennis@jauernig.email>
2021-02-25 23:46:55 +0100

    :construction: MongoDb config files

---
+++ backend/src/main/java/com/dennisjauernig/flashcards/config/MongoDbConfig.java
@@ -0,0 +1,14 @@
+package com.dennisjauernig.flashcards.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+@ConfigurationProperties (prefix = "spring.data.mongodb")
+public class MongoDbConfig {
+
+ private String host;
+ private int port;
+}
