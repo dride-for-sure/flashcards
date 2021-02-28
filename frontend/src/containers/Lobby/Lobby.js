3667764e50fc3abf1839120a52f8c48c6a7d8cb4
dennisjauernig <dennis@jauernig.email>
2021-02-28 16:18:52 +0100

    :bug: Correct uuidValidate import

--- frontend/src/containers/Lobby/Lobby.js
+++ frontend/src/containers/Lobby/Lobby.js
@@ -1,64 +1,56 @@
-import PropTypes from 'prop-types';
-import Countdown from '../../components/Cards/Countdown/Countdown';
-import GameLogo from '../../components/Cards/GameLogo/GameLogo';
-import GameTitle from '../../components/Cards/GameTitle/GameTitle';
-import PlayerList from '../../components/Cards/PlayerList/PlayerList';
-import StartGame from '../../components/Cards/StartGame/StartGame';
-import Container from './styles';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import Game from '../../components/Tiles/Game/Game';
+import Logo from '../../components/Tiles/Logo/Logo';
+import OpenGame from '../../components/Tiles/OpenGame/OpenGame';
+import Title from '../../components/Tiles/Title/Title';
+import { usePlayerDetails } from '../../contexts/playerDetails';
 
-export default function Lobby(
-  {
-    game,
-    player,
-    setPlayer,
-    countdown,
-    onGameStart,
-  },
-) {
+export default function Lobby() {
+  const [games, setGames] = useState([]);
+  const [playerName] = usePlayerDetails();
+  const history = useHistory();
+
+  const handleListGames = (data) => {
+    setGames(JSON.parse(data));
+  };
+
+  const handleOpenNewGame = (difficulty) => {
+    history.push(`/games/${difficulty}/${uuidv4()}`);
+  };
+
+  const startWebsocketConnection = () => {
+    const ws = new window.WebSocket('ws://localhost:8080/api/games') || {};
+    ws.onopen = () => {
+      console.log('Opened socket');
+    };
+
+    ws.onclose = (e) => {
+      console.log('Close socket: ', e.code, e.reason);
+    };
+
+    ws.onmessage = (e) => {
+      console.log('Message:', e);
+      handleListGames(e.data);
+    };
+  };
+
+  useEffect(() => {
+    startWebsocketConnection();
+  }, []);
+
+  if (!playerName) {
+    history.push('/');
+    return null;
+  }
+
   return (
     <>
-      <Container>
-        <GameLogo />
-        <GameTitle />
-        {countdown === '' && (
-          <StartGame
-            onGameStart={onGameStart} />
-        )}
-        {countdown !== '' && (
-          <Countdown
-            countdown={countdown} />
-        )}
-        <PlayerList
-          game={game}
-          player={player}
-          setPlayer={setPlayer} />
-      </Container>
+      <Logo />
+      <Title />
+      <OpenGame onGameOpen={handleOpenNewGame} />
+      {games.map((game) => <Game key={game.id} game={game} />)}
     </>
   );
 }
-
-Lobby.propTypes = {
-  game: PropTypes.shape({
-    id: PropTypes.string,
-    status: PropTypes.string,
-    players: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.string,
-      name: PropTypes.string,
-      points: PropTypes.number,
-    })),
-    cards: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.string,
-      level: PropTypes.number,
-      subject: PropTypes.string,
-      question: PropTypes.string,
-      choices: PropTypes.arrayOf(PropTypes.string),
-    })),
-  }).isRequired,
-  player: PropTypes.shape({
-    id: PropTypes.string,
-    name: PropTypes.string,
-  }).isRequired,
-  setPlayer: PropTypes.func.isRequired,
-  countdown: PropTypes.number.isRequired,
-  onGameStart: PropTypes.func.isRequired,
-};
