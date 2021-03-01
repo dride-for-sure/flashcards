eb60b708f5f14043bd7794a8007139f5da7c090d
dennisjauernig <dennis@jauernig.email>
2021-03-01 21:32:32 +0100

    :label: Fix backend model to be in sync with frontend

--- backend/src/main/java/com/dennisjauernig/flashcards/controller/model/StartGameDto.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/controller/model/StartGameDto.java
@@ -12,7 +12,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class StartGameDto {
 
- private String id;
  private GameMaster master;
  private Boolean start;
 
--- backend/src/main/java/com/dennisjauernig/flashcards/model/Game.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/model/Game.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Game {
 
  private String id;
+ private Difficulty difficulty;
  private GameStatus status;
  private GameMaster master;
  private List<Player> playerList;
