f384b34056b2c1014228c1a7a6ee85d3e042d9ed
dennisjauernig <dennis@jauernig.email>
2021-03-01 14:49:47 +0100

    :wastebasket: Cleanup old code

--- frontend/src/common/uuid.js
+++
@@ -1,9 +0,0 @@
-const uuid = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-  // eslint-disable-next-line no-bitwise
-  const r = (Math.random() * 16) | 0;
-  // eslint-disable-next-line no-bitwise, eqeqeq
-  const v = c == 'x' ? r : (r & 0x3) | 0x8;
-  return v.toString(16);
-});
-
-export default uuid;
