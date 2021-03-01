c19e8d641d20c416ccbbcac26fc4e2d72ef4be41
dennisjauernig <dennis@jauernig.email>
2021-03-01 20:58:10 +0100

    :card_file_box: @Document & @Id

--- backend/src/main/java/com/dennisjauernig/flashcards/controller/model/NewPlayerDto.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/controller/model/NewPlayerDto.java
@@ -4,13 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Document ( collection = "player" )
 public class NewPlayerDto {
 
+ @Id
  private String id;
  private String name;
 
