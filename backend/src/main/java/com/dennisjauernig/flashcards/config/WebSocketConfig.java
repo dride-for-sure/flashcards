f8c3445362b697d8640f5708e1e28f156f46f158
Dennis Jauernig <10451521+dride-for-sure@users.noreply.github.com>
2021-03-05 23:53:04 +0100

    Add EventListener for websocket connections, implement cleanup if game is older than 60 seconds and has no active sessionId in sessiondb (#44)

--- backend/src/main/java/com/dennisjauernig/flashcards/config/WebSocketConfig.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/config/WebSocketConfig.java
@@ -1,6 +1,10 @@
 package com.dennisjauernig.flashcards.config;
 
+import com.dennisjauernig.flashcards.events.SessionEventListener;
+import com.dennisjauernig.flashcards.service.SessionService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,6 +13,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+ @Bean
+ @Description ( "Register new player sessions" )
+ public SessionEventListener sessionEventListener ( SessionService sessionService ) {
+  return new SessionEventListener( sessionService );
+ }
 
  @Override
  public void configureMessageBroker ( MessageBrokerRegistry registry ) {
