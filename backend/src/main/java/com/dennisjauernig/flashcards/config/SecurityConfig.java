bb34b41d25943c4b25c25222f65791631072fe4c
dennisjauernig <dennis@jauernig.email>
2021-03-07 22:57:15 +0100

    :bug: JWT Bugfix

--- backend/src/main/java/com/dennisjauernig/flashcards/config/SecurityConfig.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.dennisjauernig.flashcards.filter.JwtRequestFilter;
 import com.dennisjauernig.flashcards.service.GameUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
  protected void configure ( HttpSecurity http ) throws Exception {
   http.csrf().disable()
       .authorizeRequests()
-      .mvcMatchers( HttpMethod.POST, "/authenticate/login" ).permitAll()
+      .mvcMatchers( "/authenticate" ).permitAll()
       .mvcMatchers( "/api/questions/**", "/api/games/**" ).authenticated()
       .and()
       .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS )
