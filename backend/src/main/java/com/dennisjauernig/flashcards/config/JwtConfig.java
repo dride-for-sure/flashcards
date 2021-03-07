5ff6aa94dddf8eefb2562c0731f778e9600a06af
Dennis Jauernig <10451521+dride-for-sure@users.noreply.github.com>
2021-03-07 22:59:16 +0100

    Deployment fixes+ (#57)
    * :bug: Try to fix mongoDb atlas connection...stay tuned
    
    * :bug: JWT Bugfix

--- backend/src/main/java/com/dennisjauernig/flashcards/config/JwtConfig.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/config/JwtConfig.java
@@ -1,12 +1,13 @@
 package com.dennisjauernig.flashcards.config;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
 public class JwtConfig {
 
- //@Value ( "${security.jwt.secret}" )
+ @Value ( "${security.jwt.secret}" )
  private final String jwtSecret = "top-secret";
 }
