e0b0c3ec496c3528cc1fb48239e16dff540be84e
dennisjauernig <dennis@jauernig.email>
2021-03-08 00:34:29 +0100

    Fix RequestMapping

--- backend/src/main/java/com/dennisjauernig/flashcards/controller/ReactRedirectController.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/controller/ReactRedirectController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping ( value = {
+        "/",
+        "/{x:[\\w\\-]+}",
+        "/{x:^(?!api?\\$).*$}/**/{y:[\\w\\-]+}",
+        "/{x:^(?!user?\\$).*$}/**/{y:[\\w\\-]+}",
+        "/{x:^(?!topic?\\$).*$}/**/{y:[\\w\\-]+}"} )
 public class ReactRedirectController {
 
- @RequestMapping ( value = {
-         "/",
-         "/{x:[\\w\\-]+}",
-         "/{x:^(?!api?\\$).*$}/**/{y:[\\w\\-]+}",
-         "/{x:^(?!user?\\$).*$}/**/{y:[\\w\\-]+}",
-         "/{x:^(?!topic?\\$).*$}/**/{y:[\\w\\-]+}"} )
  public String getIndex ( HttpServletRequest request ) {
   return "/index.html";
  }
