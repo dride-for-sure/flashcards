83f6178f6586ad2b7b898b014734af87bf61521c
dennisjauernig <dennis@jauernig.email>
2021-03-08 13:17:03 +0100

    :lipstick: Update Results

--- frontend/src/components/Results/ResultListItem/styled.js
+++ frontend/src/components/Results/ResultListItem/styled.js
@@ -4,12 +4,19 @@ const Container = styled.li`
   font-size: 1.2rem;
   font-style: italic;
   text-transform: lowercase;
+  
   ${(props) => props.itsMe && css`
     :after {
       content: '👈';
       margin-left: 5px;
     }
   `}
+  
+  :before {
+    content: ${(props) => props.score};
+    font-weight: 600;
+    margin-right: 8px;
+  }
 `;
 
 export default Container;
