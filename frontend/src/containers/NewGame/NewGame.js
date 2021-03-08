11b366dfb96fcaac832a8d5662bc27eae0b8358b
dennisjauernig <dennis@jauernig.email>
2021-03-08 10:16:52 +0100

    :zap: Add disconnect handler to improve lobby ux

--- frontend/src/containers/NewGame/NewGame.js
+++ frontend/src/containers/NewGame/NewGame.js
@@ -13,7 +13,7 @@ import { newGame, startGame } from '../../services/APIService';
 
 export default function NewGame() {
   const [playerDetails] = usePlayerDetails();
-  const { game, setGame } = useSocket();
+  const { game, setGame, socks, socksConnected } = useSocket();
   const [addNotification] = useNotifications();
   const { difficulty } = useParams();
   const history = useHistory();
@@ -24,11 +24,26 @@ export default function NewGame() {
       .catch(() => addNotification('Pow! Bang! Slapstick Action! But...not today!(Network Error)'));
   };
 
+  const handleGameStart = () => {
+    startGame(game.id, { id: playerDetails.id, name: playerDetails.name })
+      .then(setGame)
+      .catch(() => addNotification('Your ninja is need of sleep! Sorry. (Network Error)'));
+  };
+
+  const handleDisconnect = () => {
+    if (socksConnected) {
+      socks.sendMessage(`/api/user/${game.id}`);
+    }
+  };
+
   useEffect(() => {
     if (!uuidValidate(playerDetails.id) || !playerDetails.name.length) {
       history.push('/');
     }
     getInitialGame();
+    return (() => {
+      handleDisconnect();
+    });
   }, []);
 
   useEffect(() => {
@@ -36,12 +51,6 @@ export default function NewGame() {
       history.push(`/game/${difficulty}/${game.id}`);
     }
   }, [game]);
-
-  const handleGameStart = () => {
-    startGame(game.id, { id: playerDetails.id, name: playerDetails.name })
-      .then(setGame)
-      .catch(() => addNotification('Your ninja is need of sleep! Sorry. (Network Error)'));
-  };
 
   if (!game) {
     return (
