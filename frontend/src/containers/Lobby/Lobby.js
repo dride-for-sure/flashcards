ec2ab36bc10c2546eba3fda23d317aca0e94af18
dennisjauernig <dennis@jauernig.email>
2021-03-08 02:00:47 +0100

    :zap: Separate Lobby Websocket from socketContext to increase performance at lobby

--- frontend/src/containers/Lobby/Lobby.js
+++ frontend/src/containers/Lobby/Lobby.js
@@ -1,17 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import SockJsClient from 'react-stomp';
 import { validate as uuidValidate } from 'uuid';
 import Logo from '../../components/Tiles/Logo/Logo';
 import NewGame from '../../components/Tiles/NewGame/NewGame';
 import OpenGame from '../../components/Tiles/OpenGame/OpenGame';
 import Title from '../../components/Tiles/Title/Title';
 import { usePlayerDetails } from '../../contexts/playerDetails';
-import { useSocket } from '../../contexts/socket';
 import { listAvailableGames } from '../../services/APIService';
 
 export default function Lobby() {
   const [playerDetails] = usePlayerDetails();
-  const { games, setGames } = useSocket();
+  const [games, setGames] = useState();
   const history = useHistory();
 
   useEffect(() => {
@@ -27,8 +27,22 @@ export default function Lobby() {
     history.push(`/game/${difficulty}`);
   };
 
+  const getInitialAvailableGames = () => {
+    listAvailableGames()
+      .then(setGames)
+      .catch(() => console.error('No open games found'));
+  };
+
   return (
     <>
+      <SockJsClient
+        url="/ws"
+        topics={['/topic/games']}
+        onConnect={() => {
+          getInitialAvailableGames();
+        }}
+        onMessage={setGames}
+        debug />
       <Logo />
       <Title />
       <NewGame onGameOpen={handleOpenNewGame} />
