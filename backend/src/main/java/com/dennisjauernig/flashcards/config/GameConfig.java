e0b8b184406089c0ee11ca8834dbba27783225c2
dennisjauernig <dennis@jauernig.email>
2021-03-13 17:29:51 +0100

    :zap: Lower delay for cleanup

--- backend/src/main/java/com/dennisjauernig/flashcards/config/GameConfig.java
+++ backend/src/main/java/com/dennisjauernig/flashcards/config/GameConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Data
 public class GameConfig {
 
- private final int deleteGamesDelay = 8;
+ private final int deleteGamesDelay = 3;
  private final int maxQuestions = 25;
  private final int maxOpenGames = 20;
 }
